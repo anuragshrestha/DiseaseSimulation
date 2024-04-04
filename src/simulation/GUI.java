467c0a4c6677710f63b7b08e888f5792a1991b4d
Anurag Shrestha <ashrestha@unm.edu>
2024-04-03 18:42:31 -0600

    Addded a sample GUI

---
+++ src/simulation/GUI.java
@@ -0,0 +1,42 @@
+package simulation;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        GridPane gridPane = new GridPane();
+
+        // Create Labels
+        Label label1 = new Label("Label 1");
+        Label label2 = new Label("Label 2");
+
+        // Add Labels to GridPane
+        gridPane.add(label1, 0, 0);
+        gridPane.add(label2, 1, 0);
+
+        // Setting the padding around the grid
+        gridPane.setPadding(new javafx.geometry.Insets(10, 10, 10, 10));
+
+        gridPane.setVgap(5);
+        gridPane.setHgap(5);
+
+        // Create a Scene with GridPane as its root node
+        Scene scene = new Scene(gridPane, 300, 200);
+
+        // Set the scene on the primary stage
+        primaryStage.setTitle("GridPane Example");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
